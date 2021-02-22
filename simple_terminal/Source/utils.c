cbd84738c88e766e348c85341cc6f3788d6b7118
Enmanuel Parache <enmanuelparache@gmail.com>
2021-02-21 21:47:35 -0400

    Fix typo

--- simple_terminal/Source/utils.c
+++ simple_terminal/Source/utils.c
@@ -12,7 +12,7 @@ void clear_line(void) {
 /**
  * @param str1 String input one
  * @param str2 String input two
- * @return 1 if str1 and str2 are equal otherwiser return 0
+ * @return 1 if str1 and str2 are equal otherwise return 0
  **/
 int equal_str(char str1[], char str2[]) {
   return strcmp(str1, str2) == 0;
